34f1ea9d76c65d38d79674d2f4b2402b2e1b9da2
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-07-26 15:33:29 +0200

    Add Phone Number Authentication
    Co-authored-by: Micheline Medhat <MichelineMedhat@gmail.com>
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- app/src/main/java/com/minim/messenger/SigningActivity.kt
+++ app/src/main/java/com/minim/messenger/SigningActivity.kt
@@ -1,24 +1,86 @@
 package com.minim.messenger
 
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.signing_activity.*
+import java.util.concurrent.TimeUnit
 
 class SigningActivity : AppCompatActivity() {
+
+    //private val auth = FirebaseAuth.getInstance()
+
+//    companion object {
+//        var storedVerificationId: String = ""
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signing_activity)
 
         sendVerificationCodeButton.setOnClickListener {
-            if (!phoneNumberEditText.text.toString().isNullOrEmpty()) {
-                val i = Intent(this, VerificationActivity::class.java)
-                startActivity(i)
-            } else {
-                Toast.makeText(this, "Enter Phone Number", Toast.LENGTH_LONG).show()
-            }
+            startPhoneNumberVerification(phoneNumberEditText.text.toString())
         }
     }
+
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            phoneNumber, // Phone number to verify
+            60, // Timeout duration
+            TimeUnit.SECONDS, // Unit of timeout
+            this@SigningActivity, // Activity (for callback binding)
+            callbacks
+        )
+    }
+
+
+    private val callbacks =
+        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d(ContentValues.TAG, "onVerificationCompleted:$credential")
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+
+                val mainIntent = Intent(this@SigningActivity, MainActivity::class.java)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(mainIntent)
+                finish()
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.w(ContentValues.TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                    phoneNumberEditText.error = "Invalid phone number."
+                    Toast.makeText(applicationContext, "Invalid phone number", Toast.LENGTH_LONG).show()
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                    Toast.makeText(applicationContext, "Try again Later", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onCodeSent(verificationId: String?, token: PhoneAuthProvider.ForceResendingToken) {
+                Log.d(ContentValues.TAG, "onCodeSent:" + verificationId!!)
+
+                // Save verification ID and resending token so we can use them later
+                val i = Intent(this@SigningActivity, VerificationActivity::class.java)
+                    .putExtra("verificationId", verificationId)
+                startActivity(i)
+                finish()
+            }
+        }
 }
