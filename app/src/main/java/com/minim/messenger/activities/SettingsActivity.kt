079107d7a4352a54dedb3a2633c71815a5765310
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-07-29 17:43:40 +0200

    Add Username Validation Rules

--- app/src/main/java/com/minim/messenger/activities/SettingsActivity.kt
+++ app/src/main/java/com/minim/messenger/activities/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.minim.messenger.activities
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -17,24 +18,25 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.settings_activity)
 
         saveSettingsButton.setOnClickListener {
-
-            when {
-                TextUtils.isEmpty(userNameEditText.text) -> userNameEditText.error = "Required field."
-                userNameEditText.text.contains(" ") -> userNameEditText.error = "username can't contain spaces."
-                else -> {
-                    addUser(userNameEditText.text.toString())
-                    launchApp()
-                }
+            if (validateUsername(userNameEditText)) {
+                addUser(userNameEditText.text.toString())
             }
-
         }
     }
 
     private fun addUser(username: String) {
         val authUser = FirebaseAuth.getInstance().currentUser ?: return
         val user = User(authUser.uid, authUser.phoneNumber!!, username)
-        FirebaseFirestore.getInstance().collection("users")
-            .document(user.phoneNumber).set(user)
+        FirebaseFirestore.getInstance().collection("users").document(username).get()
+            .addOnCompleteListener {
+                if (it.result!!.exists()) {
+                    userNameEditText.error = "@${userNameEditText.text} already in use. Try another."
+                } else {
+                    FirebaseFirestore.getInstance().collection("users")
+                        .document(user.username!!).set(user)
+                    launchApp()
+                }
+            }
     }
 
     private fun launchApp() {
@@ -43,6 +45,23 @@ class SettingsActivity : AppCompatActivity() {
             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
         finish()
+    }
+
+    companion object {
+        fun validateUsername(editText: EditText): Boolean {
+            when {
+                editText.text.isEmpty() -> editText.error =
+                    "Required field."
+                editText.text matches "[^a-zA-Z0-9]+".toRegex() -> editText.error =
+                    "Use only letters and numbers."
+                editText.text.length > 18 -> editText.error =
+                    "Use less than 18 characters."
+                else ->
+                    return true
+            }
+            return false
+
+        }
     }
 
 }
