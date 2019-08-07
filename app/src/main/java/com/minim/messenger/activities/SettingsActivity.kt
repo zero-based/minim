6fff09bedc15fb72496f3466583fc3492f8fe9d7
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-08-07 16:42:10 +0200

    Add Help Button to Settings

--- app/src/main/java/com/minim/messenger/activities/SettingsActivity.kt
+++ app/src/main/java/com/minim/messenger/activities/SettingsActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Toast
 import com.minim.messenger.R
 import kotlinx.android.synthetic.main.activity_settings.*
+import android.content.Intent
+import android.net.Uri
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -15,6 +17,12 @@ class SettingsActivity : AppCompatActivity() {
 
         val notSupportedToast = Toast.makeText(this, "Not Supported Yet!", Toast.LENGTH_SHORT)
         update_button.setOnClickListener { notSupportedToast.show() }
+        help_button.setOnClickListener {
+            val url = "https://michaelsafwathanna.github.io/minim/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
         delete_button.setOnClickListener { notSupportedToast.show() }
         sign_out_button.setOnClickListener { notSupportedToast.show() }
 
